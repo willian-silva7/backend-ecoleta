4fdb3fcaf4c54b69d259369e866d555eb85032fe
willian <will.oliver1994@hotmail.com>
2020-09-12 01:09:47 +0100

    added item route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,19 @@
 import express from 'express';
+import knex from '../database/connection';
 
 const router = express.Router();
 
-router.get('/users', (request, response) => {
-  return response.json('Hello World Caralho');
+router.get('/items', async (request, response) => {
+  const items = await knex('items').select('*');
+
+  const serializedItems = items.map(item => {
+    return {
+      title: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`,
+    };
+  });
+
+  return response.json(serializedItems);
 });
 
 router.post('/users', (request, response) => {
